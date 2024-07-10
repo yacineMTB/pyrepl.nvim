17c04163161d76fa71faf74268a7e88bab83af98
Yacine Brahimi <yacine.bram@gmail.com>
2024-07-09 23:26:53 -0400

    simple interactive repl

---
+++ lua/pyrepl.lua
@@ -0,0 +1,37 @@
+local M = {}
+local curl = require('plenary.curl')
+
+M.config = {
+  url = 'http://localhost:5000/execute'
+}
+
+function M.setup(opts)
+  M.config = vim.tbl_extend('force', M.config, opts or {})
+end
+
+function M.send_to_repl(code)
+  local response = curl.post(M.config.url, {
+    body = vim.fn.json_encode({code = code}),
+    headers = {
+      content_type = 'application/json',
+    },
+  })
+
+  local result = vim.fn.json_decode(response.body)
+  if result.error then
+    print("Error:", result.error)
+  elseif result.output then
+    print("Output:", result.output)
+  end
+end
+
+function M.run_selected_lines()
+  local start_line = vim.fn.line("'<")
+  local end_line = vim.fn.line("'>")
+  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+  local code = table.concat(lines, "\n")
+  M.send_to_repl(code)
+end
+
+return M
+
